c21671bc71fd3d191e79c6817c00e2eed18808e9
GulievCorp <guliev@internet.ru>
2022-08-08 09:20:12 +0300

    add lodash and use debounce in fetchSearch

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -2,15 +2,23 @@ import React from 'react'
 import styles from './Search.module.scss'
 import { useDispatch } from 'react-redux/es/exports';
 import { changeSearch } from '../../store/slices/PizzaSlice';
+import { debounce } from 'lodash';
 
 export default function Search() {
   const dispatch = useDispatch();
+  const inputRef = React.useRef('');
+
+  const fetchSearch = React.useCallback(
+    debounce((e) => {
+      dispatch(changeSearch(e.target.value));
+  }, 1000), []);
 
   return (
     <div>
       <input
+        ref={inputRef}
         onChange={(e) => {
-          dispatch(changeSearch(e.target.value));
+          fetchSearch(e);
         }} 
         className={styles.searchBtn} 
         type="text" 
