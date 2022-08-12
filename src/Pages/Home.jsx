9292fc25fb3b7af49d93da502993c07c7eb53044
GulievCorp <guliev@internet.ru>
2022-08-12 16:15:54 +0300

    fix

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,21 +5,44 @@ import MyLoader from '../components/Loader';
 import PizzaBlock from '../components/PizzaBlock'
 import { getPizzas } from '../service/main.service';
 import Pagination from '../components/Pagination';
-import { useSelector, useDispatch } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { setFilters } from '../store/slices/PizzaSlice.js';
+import qs  from 'qs'
 
 const Home = () => {
   const { categorie, sort, search } = useSelector((state) => state.pizza);
   const [Pizzas, setPizzas] = React.useState();
   const [isLoading, setIsLoading] = React.useState(false);
   const [ currentPage, setCurrentPage ] = React.useState(1);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   React.useEffect(() => {
-    getPizzas(setPizzas, setIsLoading, 1);
+    if(window.location.search){
+      const queryObj = qs.parse(window.location.search.substring(1))
+      console.log(queryObj);
+      dispatch(setFilters(qs.parse(queryObj)));
+    }
     window.scrollTo(0, 0);
-  }, []);
+  }, [window.location.search]);
 
   React.useEffect(() => {
     getPizzas(setPizzas, setIsLoading, currentPage, categorie, sort, search);
+  }, [currentPage, sort, search, categorie]);
+
+  React.useEffect(() => {
+    if(window){
+      const queryObj = {
+        page: currentPage,
+        sortBy: sort,
+        search: search,
+        category: categorie
+      };
+      const queryStr = qs.stringify(queryObj);
+
+      navigate(`?${queryStr}`);
+    }
   }, [currentPage, sort, search, categorie]);
 
   return <>
--- src/store/slices/PizzaSlice.js
+++ src/store/slices/PizzaSlice.js
@@ -1,13 +1,14 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
+    page: 1,
     categorie: 0,
     sort: 'rating',
     search: ''
 };
 
 export const PizzaReducer = createSlice({
-    name: 'filter',
+    name: 'categories',
     initialState,
     reducers: {
         changeCategories: (state, action) => {
@@ -18,10 +19,16 @@ export const PizzaReducer = createSlice({
         },
         changeSearch: (state, action) => {
             state.search = action.payload
+        },
+        setFilters: (state, action) => {
+          state.page = action.payload.page
+          state.categorie = action.payload.category
+          state.sort = action.payload.sortBy
+          state.search = action.payload.search
         }
     }
 })
 
-export const { changeCategories, changeSort, changeSearch } = PizzaReducer.actions
+export const { changeCategories, changeSort, changeSearch, setFilters } = PizzaReducer.actions
 
 export default PizzaReducer.reducer;
