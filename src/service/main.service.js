43057402592bbd1b739de5430b04909e695d3820
guliev <guliev@internet.ru>
2022-08-26 17:51:36 +0300

    delete old comment

--- src/service/main.service.js
+++ src/service/main.service.js
@@ -1,32 +1,3 @@
-// const mockApi = `https://62d1010cd9bf9f170590bf69.mockapi.io/Items?`;
-
-// export const getPizzas = async (
-//   setPizzas,
-//   setIsLoading,
-//   page,
-//   categoryId,
-//   sortProperty,
-//   searchValue
-//   ) => {
-//   const url = `page=${page}&` + 
-//   `${categoryId ? `&category=${categoryId}` : ''}` + 
-//   `${sortProperty ? `&sortBy=${sortProperty}&order=asc` : `&sortBy=rating&order=asc`}` +
-//   `${searchValue ? `&search=${searchValue}` : ''}`;
-
-//   setIsLoading(true);
-//   await fetch(mockApi + url)
-//     .then((res) => {
-//       return res.json();
-//     })
-//     .then((json) => {
-//       if(json.length > 0) setPizzas(json);
-//       setIsLoading(false);
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//     })
-// }; 
-
 import axios from "axios";
 
 const mockApi = `https://62d1010cd9bf9f170590bf69.mockapi.io/Items?`;
@@ -51,6 +22,6 @@ export const getPizzas = async (
     await setPizzas(res.data);
     await setIsLoading(false);
   } catch(err) {
-    console.log(err);
+    alert(err.mesage);
   }
 };
