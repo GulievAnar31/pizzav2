ab8e70a2cb11b61ba825a8cfdb10bd888e8b215d
guliev <guliev@internet.ru>
2022-08-26 17:49:05 +0300

    install axios and add try catch in service

--- src/service/main.service.js
+++ src/service/main.service.js
@@ -1,3 +1,34 @@
+// const mockApi = `https://62d1010cd9bf9f170590bf69.mockapi.io/Items?`;
+
+// export const getPizzas = async (
+//   setPizzas,
+//   setIsLoading,
+//   page,
+//   categoryId,
+//   sortProperty,
+//   searchValue
+//   ) => {
+//   const url = `page=${page}&` + 
+//   `${categoryId ? `&category=${categoryId}` : ''}` + 
+//   `${sortProperty ? `&sortBy=${sortProperty}&order=asc` : `&sortBy=rating&order=asc`}` +
+//   `${searchValue ? `&search=${searchValue}` : ''}`;
+
+//   setIsLoading(true);
+//   await fetch(mockApi + url)
+//     .then((res) => {
+//       return res.json();
+//     })
+//     .then((json) => {
+//       if(json.length > 0) setPizzas(json);
+//       setIsLoading(false);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     })
+// }; 
+
+import axios from "axios";
+
 const mockApi = `https://62d1010cd9bf9f170590bf69.mockapi.io/Items?`;
 
 export const getPizzas = async (
@@ -13,13 +44,13 @@ export const getPizzas = async (
   `${sortProperty ? `&sortBy=${sortProperty}&order=asc` : `&sortBy=rating&order=asc`}` +
   `${searchValue ? `&search=${searchValue}` : ''}`;
 
-  setIsLoading(true);
-  await fetch(mockApi + url)
-    .then((res) => {
-      return res.json();
-    })
-    .then((json) => {
-      if(json.length > 0) setPizzas(json);
-      setIsLoading(false);
-    });
-}; 
+  await setIsLoading(true);
+
+  try {
+    const res = await axios.get(mockApi + url);
+    await setPizzas(res.data);
+    await setIsLoading(false);
+  } catch(err) {
+    console.log(err);
+  }
+};
